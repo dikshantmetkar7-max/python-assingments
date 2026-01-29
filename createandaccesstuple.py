085a24c463cb965b976b39dd555cf677ce281906
dikshantmetkar7-max <dikshantmetkar7@gmail.com>
2026-01-29 14:45:57 +0530

    Create createandaccesstuple.py

---
+++ createandaccesstuple.py
@@ -0,0 +1,14 @@
+tuple_create = (1, "hello", 3.5, True)
+
+print(f"Created tuple: {tuple_create}")
+
+
+first_element = tuple_create[0]
+second_element = tuple_create[1]
+last_element = tuple_create[-1] 
+
+print(f"First element: {first_element}")
+print(f"Second element: {second_element}")
+print(f"Last element: {last_element}")
+
+print("-" * 20) 
