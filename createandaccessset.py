583ad3ce9cc403613fbd186b7c59e08048a5a36b
dikshantmetkar7-max <dikshantmetkar7@gmail.com>
2026-01-29 14:44:02 +0530

    Create createandaccessset.py

---
+++ createandaccessset.py
@@ -0,0 +1,10 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print("Set 1 elements:")
+for element in set1:
+    print(element)
+
+print("\nSet 2 elements:")
+for element in set2:
+    print(element)
